e8a443ca90e47186ac520745463071cb3a11d360
TristanK <tris.klein13@gmail.com>
2026-09-22 11:19:42 +0100

    Moved code in Main to HttpServer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import models.HttpResponse;
 import models.HttpStatus;
+import server.HttpServer;
 import server.RequestHandler;
 import util.Util;
 
@@ -8,51 +9,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
-//TODO: Add enums for the methods
-
 public class Main {
-     static int port = 9191; //Specifies the port to listen on
-     static String crlf = "\r\n"; //Specifies the carriage return and line feed characters used in requests
-
-    public static void main(String[] args) {
-        try {
-            System.out.println("Server boot");
-
-            //Open server socket on the port to listen for connections
-            ServerSocket serverSocket = new ServerSocket(port);
-
-            //Accept connection from client
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client connected: " + clientSocket.getInetAddress());
-
-            //Read message from client
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            RequestHandler requestHandler = new RequestHandler(reader);
-
-            OutputStream outputStream = clientSocket.getOutputStream(); //Get output stream to send data to client
-
-            byte[] body = "Hello World!".getBytes(StandardCharsets.UTF_8); //Splitting it into bytes
-
-            String response1 =
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: text/plain\r\n" +
-                            "Content-Length: " + body.length + "\r\n" +
-                            "\r\n";
-
-            HttpResponse responseObj = new HttpResponse(HttpStatus.OK, "text/plain", "close", body);
-
-            outputStream.write(responseObj.responseBytes);
-            outputStream.write(responseObj.body);
-            outputStream.flush();
-
-            //Close the sockets
-            reader.close();
-            clientSocket.close();
-            serverSocket.close();
-
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage()); //Output the error
-        }
+    public static void main(String[] args) throws IOException {
+        HttpServer server = new HttpServer(9191);
+        server.startServer();
     }
 }
 
--- src/main/java/server/HttpServer.java
+++ src/main/java/server/HttpServer.java
@@ -1,5 +1,54 @@
 package server;
 
-public class HttpServer {
+import models.HttpResponse;
+import models.HttpStatus;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class HttpServer {
+    private int port; //Specifies the port to listen on
+    static String crlf = "\r\n"; //Specifies the carriage return and line feed characters used in requests
+
+    public HttpServer(int port) {
+        this.port = port;
+    }
+
+    public void startServer() throws IOException {
+        try {
+            //Open server socket on the port to listen for connections
+            ServerSocket serverSocket = new ServerSocket(this.port);
+
+            //Accept connection from client
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getInetAddress()); //TODO: change to actual IP address
+
+            //Read message from client
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            RequestHandler requestHandler = new RequestHandler(reader);
+
+            OutputStream outputStream = clientSocket.getOutputStream(); //Get output stream to send data to client
+
+            byte[] body = "Hello World!".getBytes(StandardCharsets.UTF_8); //Splitting it into bytes
+
+            HttpResponse responseObj = new HttpResponse(HttpStatus.OK, "text/plain", "close", body);
+
+            outputStream.write(responseObj.responseBytes);
+            outputStream.write(responseObj.body);
+            outputStream.flush();
+
+            //Close the sockets
+            reader.close();
+            clientSocket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage()); //Output the error
+        }
+    }
 }
